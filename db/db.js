4c2129f4dec403d2e658bce4605965160d385cf3
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-12-25 22:26:34 +0600

    complete updateById and deleteById

--- db/db.js
+++ db/db.js
@@ -1,65 +1,100 @@
-const Ticket = require('../models/Ticket');
+/** @format */
 
-class MyDB{
-    constructor(){
-        this.tickets = []
+const Ticket = require('../models/Ticket')
+
+class MyDB {
+  constructor() {
+    this.tickets = []
+  }
+
+  /**
+   * cretae and save a new Ticket
+   * @param {string} username
+   * @param {number} price
+   * @returns {Ticket} returns a new Ticket object
+   */
+  create(username, price) {
+    const ticket = new Ticket(username, price)
+    this.tickets.push(ticket)
+    return ticket
+  }
+
+  /**
+   * create a multiple ticket for a single user
+   * @param {string} username
+   * @param {number} price
+   * @param {number} quantity
+   * @returns {Ticket[]} returns an array of Ticket objects
+   */
+  bulkCreate(username, price, quantity) {
+    const result = []
+    for (let i = 0; i < quantity; i++) {
+      const ticket = this.create(username, price)
+      result.push(ticket)
     }
+    return result
+  }
 
-    /**
-     * cretae and save a new Ticket
-     * @param {string} username 
-     * @param {number} price 
-     * @returns {Ticket} returns a new Ticket object
-     */
-    create(username,price){
-        const ticket = new Ticket(username,price);
-        this.tickets.push(ticket);
-        return ticket;
+  /**
+   * returns available tickets
+   * @returns {Ticket[]} returns an array of Ticket objects
+   */
+  find() {
+    return this.tickets
+  }
+
+  /**
+   * find tickets by ticket id
+   * @param {string} ticketId
+   * @returns {Ticket} returns a Ticket object
+   */
+  findById(ticketId) {
+    return this.tickets.find((ticket) => ticket.id === ticketId)
+  }
+
+  /**
+   * find all tickets for a given user
+   * @param {string} username
+   * @returns {Ticket[]} returns an array of Ticket objects
+   */
+  findByUsername(username) {
+    return this.tickets.filter((ticket) => ticket.username === username)
+  }
+
+  /**
+   * update ticket
+   * @param {string} ticketId
+   * @param {{username: string, price: number}} ticketBody
+   * @return {Ticket} returns a Ticket object
+   */
+  updateById(ticketId, ticketBody) {
+    const ticket = this.findById(ticketId)
+    if (ticket) {
+      ticket.updateAt = new Date()
+      Object.assign(ticket, ticketBody)
+      return ticket
     }
+  }
 
-    /**
-     * create a multiple ticket for a single user
-     * @param {string} username
-     * @param {number} price
-     * @param {number} quantity
-     * @returns {Ticket[]} returns an array of Ticket objects
-     */
-    bulkCreate(username,price,quantity){
-        const result = [];
-        for(let i = 0; i < quantity; i++){
-            this.create(username,price);
-            result.push(this.tickets[this.tickets.length-1]);
-        }
-        return result;
+  /**
+   * delete a ticket
+   * @param {string} ticketId
+   * @return {boolean} Boolean
+   */
+  deleteById(ticketId) {
+    const ticket = this.findById(ticketId)
+    if (ticket) {
+      this.tickets = this.tickets.filter((t) => t.id !== ticketId)
+      return true
     }
+    return false
+  }
 
-    }
-
-    // return all tickets
-    find(){
-
-    }
-
-    // single ticket
-    findById(){
-
-    }
-
-    // update ticket instanceof
-    updateById(){
-
-    }
-
-    //delete ticket
-    deleteById(){
-
-     }
-
-     //raffle draw
-     draw(){
-
-     }
+  /**
+   * find winners
+   */
+  draw() {}
 }
 
-const myDB = new MyDB();
-module.exports = myDB;
+const myDB = new MyDB()
+module.exports = myDB
--- models/Ticket.js
+++ models/Ticket.js
@@ -1,17 +1,19 @@
-const shortid = require('shortid');
+/** @format */
 
-class Ticket{
-    /**
-     * constructor function
-     * @param {string} username 
-     * @param {number} price 
-     */
-    constructor(username,price){
-        this.id = shortid.generate();
-        this.username = username;
-        this.price = price;
-        this.createAt  = new Date();
-        this.updateAt = new Date();
-    }
+const shortid = require('shortid')
+
+class Ticket {
+  /**
+   * constructor function
+   * @param {string} username
+   * @param {number} price
+   */
+  constructor(username, price) {
+    this.id = shortid.generate()
+    this.username = username
+    this.price = price
+    this.createAt = new Date()
+    this.updateAt = new Date()
+  }
 }
-module.exports = Ticket;
+module.exports = Ticket
